47b9e4328c4e6c98377cf262224b3087df46cec2
Novikov Ivan <ivan.novikov.1990@list.ru>
2022-03-20 20:31:33 +0300

    refactoring imports with "default"

--- src/components/project-input.ts
+++ src/components/project-input.ts
@@ -1,7 +1,7 @@
 import { Autobind } from "../decorators/autobind.js";
 import { projectState } from "../state/project-state.js";
-import { Validatable, validate } from "../util/validation.js";
-import { Component } from "./base-component.js";
+import * as Validation from "../util/validation.js";
+import Component from "./base-component.js";
 
 //Project Input Class
 export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
@@ -35,16 +35,16 @@ export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
         const enteredDescription = this.desctiptionInputElement.value;
         const enteredPeople = this.peopleInputElement.value;
 
-        const titleValidatable: Validatable = {
+        const titleValidatable: Validation.Validatable = {
             value: enteredTitle,
             required: true,
         };
-        const descriptionValidatable: Validatable = {
+        const descriptionValidatable: Validation.Validatable = {
             value: enteredDescription,
             required: true,
             minLength: 5,
         };
-        const peopleValidatable: Validatable = {
+        const peopleValidatable: Validation.Validatable = {
             value: +enteredPeople,
             required: true,
             min: 1,
@@ -52,9 +52,9 @@ export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
         };
 
         if (
-            !validate(titleValidatable) ||
-            !validate(descriptionValidatable) ||
-            !validate(peopleValidatable)
+            !Validation.validate(titleValidatable) ||
+            !Validation.validate(descriptionValidatable) ||
+            !Validation.validate(peopleValidatable)
         ) {
             alert("Invalid input, please try again!");
             return;
