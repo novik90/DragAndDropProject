3ac1c4b3f567c6d6741e11f11b4f6cc534841f31
Novikov Ivan <ivan.novikov.1990@list.ru>
2022-03-20 15:58:10 +0300

    added projectlist class

--- src/app.ts
+++ src/app.ts
@@ -64,6 +64,42 @@ function Autobind(
 }
 //
 
+//ProjdectList Class
+class ProjdectList {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLElement;
+
+    constructor(private type: "active" | "finished") {
+        this.templateElement = document.getElementById(
+            "project-list"
+        ) as HTMLTemplateElement;
+        this.hostElement = document.getElementById("app") as HTMLDivElement;
+
+        const importedNode = document.importNode(
+            this.templateElement.content,
+            true
+        );
+        this.element = importedNode.firstElementChild as HTMLElement;
+        this.element.id = `${this.type}-projects`;
+
+        this.attach();
+        this.renderContent();
+    }
+
+    private renderContent() {
+        const listId = `${this.type}-project-list`;
+        this.element.querySelector("ul")!.id = listId;
+        this.element.querySelector("h2")!.textContent =
+            this.type.toUpperCase() + " PROJECTS";
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement("beforeend", this.element);
+    }
+}
+
+//Project Class
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
@@ -104,14 +140,26 @@ class ProjectInput {
         const enteredDescription = this.desctiptionInputElement.value;
         const enteredPeople = this.peopleInputElement.value;
 
+        const titleValidatable: Validatable = {
+            value: enteredTitle,
+            required: true,
+        };
+        const descriptionValidatable: Validatable = {
+            value: enteredDescription,
+            required: true,
+            minLength: 5,
+        };
+        const peopleValidatable: Validatable = {
+            value: +enteredPeople,
+            required: true,
+            min: 1,
+            max: 5,
+        };
+
         if (
-            validate({ value: enteredTitle, required: true, minLength: 5 }) &&
-            validate({
-                value: enteredDescription,
-                required: true,
-                minLength: 5,
-            }) &&
-            validate({ value: enteredPeople, required: true, minLength: 5 })
+            !validate(titleValidatable) ||
+            !validate(descriptionValidatable) ||
+            !validate(peopleValidatable)
         ) {
             alert("Invalid input, please try again!");
             return;
@@ -132,7 +180,7 @@ class ProjectInput {
         const userInput = this.gatherUserInput();
         if (Array.isArray(userInput)) {
             const [title, desc, people] = userInput;
-            console.log(title, desc, people);
+
             this.clearInputs();
         }
     }
@@ -147,3 +195,5 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput();
+const activePrjList = new ProjdectList("active");
+const finishedPrjList = new ProjdectList("finished");
