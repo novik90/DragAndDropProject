230f6e92dcc15a745c3f3055aa280e09dec79504
Novikov Ivan <ivan.novikov.1990@list.ru>
2022-03-20 19:41:13 +0300

    restructuring project with ES Modules

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,6 @@
-/// <reference path="models/drag-drop.ts" />
-/// <reference path="models/project.ts" />
-/// <reference path="state/project-state.ts" />
-/// <reference path="util/validation.ts" />
-/// <reference path="decorators/autobind.ts" />
-/// <reference path="components/project-input.ts" />
-/// <reference path="components/project-list.ts" />
+import { ProjectInput } from "./components/project-input.js";
+import { ProjdectList } from "./components/project-list.js";
 
-namespace App {
-    new ProjectInput();
-    new ProjdectList("active");
-    new ProjdectList("finished");
-}
+new ProjectInput();
+new ProjdectList("active");
+new ProjdectList("finished");
